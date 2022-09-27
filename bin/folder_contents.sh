9bdef508394b63fe1465b55c00f08f115b39981f
David Michael <1.david.michael@gmail.com>
2022-09-27 10:18:02 -0500

    Correct shellcheck errors

--- bin/folder_contents.sh
+++ bin/folder_contents.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -ex
-# shellcheck disable=SC2016,SC2288
+# shellcheck disable=SC2016,SC2288,SC2154
 
 eval "$(jq -r '@sh "export code_location=\(.code_location) output_dir=\(.output_dir)"')"
-echo "code_location: $code_location output_dir: $output_dir" > /tmp/output.txt
 
 # Convert json argument into environment variables
 # for s in $(echo "$@" | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" ); do
@@ -20,7 +19,7 @@ else
 fi
 
 # Take md5 from each object inside the program and then take a md5 of that output
-MD5_OUTPUT="$(find "$code_location" -type f | xargs $MD5_PROGRAM | $MD5_PROGRAM | awk '{ print $1 }' )"
+MD5_OUTPUT="$(find "$code_location" -type f -print0| xargs -0 $MD5_PROGRAM | $MD5_PROGRAM | awk '{ print $1 }' )"
 
 # Output result as JSON back to terraform
 jq --null-input \
